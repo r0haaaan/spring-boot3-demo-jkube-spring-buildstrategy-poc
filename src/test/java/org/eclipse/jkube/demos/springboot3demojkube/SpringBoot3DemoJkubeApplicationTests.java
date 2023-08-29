d1973fcc51802aabbd32353c14382cbe1c1af277
Rohan Kumar <rohaan@redhat.com>
2023-08-29 13:29:49 +0530

    Initial Commit
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ src/test/java/org/eclipse/jkube/demos/springboot3demojkube/SpringBoot3DemoJkubeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eclipse.jkube.demos.springboot3demojkube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3DemoJkubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
